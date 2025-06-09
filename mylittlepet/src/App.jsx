7a8561dadc955abcbd3471d19dd5e2f3a0a7231b
hgzisme <bluax1604@gmail.com>
2025-06-09 09:13:23 +0700

    update with some error

--- mylittlepet/src/App.jsx
+++ mylittlepet/src/App.jsx
@@ -3,30 +3,31 @@ import { AuthProvider } from './contexts/AuthContextV2';
 import { NotificationProvider } from './contexts/NotificationContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
+import ConnectionStatus from './components/ConnectionStatus';
 import Login from './pages/manager/Login';
 import Register from './pages/manager/Register';
 import PlayersV2 from './pages/manager/PlayersV2';
-import Pets from './pages/manager/Pets';
-import Items from './pages/manager/Items';
+import PetsV2 from './pages/manager/PetsV2';
+import ItemsV2 from './pages/manager/ItemsV2';
 
 function App() {
   return (
     <AuthProvider>
       <NotificationProvider>
         <Router>
-          <Routes>
-            <Route path="/login" element={<Login />} />
+          <Routes>            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/*" element={<ProtectedRoute>
-              <Layout>
-                <Routes>
-                  <Route path="/" element={<Navigate to="/players" replace />} />
-                  <Route path="/players" element={<PlayersV2 />} />
-                  <Route path="/pets" element={<Pets />} />
-                  <Route path="/items" element={<Items />} />
-                </Routes>
-              </Layout>
-            </ProtectedRoute>
+            <Route path="/*" element={
+              <ProtectedRoute>
+                <Layout>
+                  <Routes>
+                    <Route path="/" element={<Navigate to="/players" replace />} />
+                    <Route path="/players" element={<PlayersV2 />} />
+                    <Route path="/pets" element={<PetsV2 />} />
+                    <Route path="/items" element={<ItemsV2 />} />
+                  </Routes>
+                </Layout>
+              </ProtectedRoute>
             } />
           </Routes>
         </Router>
