56aa76f5ac9d326dfac7d7b213bc9048785fe103
hgzisme <bluax1604@gmail.com>
2025-06-11 09:18:44 +0700

    refactor

--- mylittlepet/src/App.jsx
+++ mylittlepet/src/App.jsx
@@ -3,7 +3,7 @@ import { AuthProvider, useAuth } from './contexts/AuthContextV2';
 import { NotificationProvider } from './contexts/NotificationContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
-import ConnectionStatus from './components/ConnectionStatus';
+// import ConnectionStatus from './components/ConnectionStatus'; // Removed as requested
 import Login from './pages/manager/Login';
 import Register from './pages/manager/Register';
 import PlayersV2 from './pages/manager/PlayersV2';
@@ -64,7 +64,7 @@ function App() {
     <AuthProvider>
       <NotificationProvider>
         <Router>
-          <ConnectionStatus />
+          {/* <ConnectionStatus /> */}
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
